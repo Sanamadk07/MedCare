3915d983232e0c06113b2267d1c08ac6828c6eb9
Sanam Adhikari <105903734+Sanamadk07@users.noreply.github.com>
2023-09-06 10:57:56 +0545

    Update orders.php

--- admin/orders.php
+++ admin/orders.php
@@ -13,7 +13,7 @@ include "includes/head.php";
     ?>
     <main class="col-md-9 ms-sm-auto col-lg-10 px-md-4">
         <?php
-        // message();
+        message();
         ?>
         <div class="container">
             <div class="row align-items-start">
